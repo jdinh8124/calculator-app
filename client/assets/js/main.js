78ea35217575d9cf3d6c8097f1e42cceb7879148
James Dinh <mrjames8124@gmail.com>
2019-11-18 16:57:12 -0800

    finished display and storage

--- client/assets/js/main.js
+++ client/assets/js/main.js
@@ -5,18 +5,45 @@ function applyClickHandlers(){
   $("#operator-column").on("click", ".operator", operatorButtonHandler )
   $("#equals").on("click", equalsButtonHandler)
   function numberButtonHandler(event){
-    console.log(event);
+    var inputtedNumber = "";
+    inputtedNumber = $(event.currentTarget).find("p").text();
+    console.log(inputtedNumber);
+    stringNumberToPush += inputtedNumber;
+    displayArray.push(inputtedNumber);
+    console.log(displayArray)
+    updateDisplay();
+
+
   }
 
 
   function operatorButtonHandler(event){
     console.log(event);
+    var inputtedOperator ="";
+    inputtedOperator = $(event.currentTarget).find("p").text();
+    //Might be missing a step += into a variable? but I am not sure
+    displayArray.push(inputtedOperator);
+    updateDisplay();
+    calculationArray.push(stringNumberToPush);
+    console.log(calculationArray);
+    calculationArray.push(inputtedOperator);
+    console.log(calculationArray);
+    stringNumberToPush = "";
   }
 
   function equalsButtonHandler(event){
     console.log(event);
+    calculationArray.push(stringNumberToPush);
+    stringNumberToPush = "";
+    displayArray = [];
+    updateDisplay();
   }
 
+
+  function updateDisplay() {
+    var displayText = displayArray.join("");
+    $("#display-text").text(displayText);
+  }
 
 }
 
